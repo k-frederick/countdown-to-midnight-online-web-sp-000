61f804ddf6161dce953a43fd0e65cd06d26b80c7
Kristina Frederick <kristinafrederick@icloud.com>
2020-05-13 21:51:07 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,9 @@
 #write your code here
 
 def countdown
-  countdown = 10
-  while countdown > 0
-    puts "#{countdown} SECOND(S)!"
-    countdown -= 1
+  countdown_output = 10
+  while countdown_output > 0
+    puts "#{countdown_output} SECOND(S)!"
+    countdown_output -= 1
   end
 end
