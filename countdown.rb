e8e0b5aa6ba432bd51c435025ed31ce4536ca846
Kristina Frederick <kristinafrederick@icloud.com>
2020-05-13 22:32:44 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -10,5 +10,11 @@ def countdown(number)
 end
 
 def countdown_with_sleep(number)
-  
+  number = 10
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    sleep 1
+    number -= 1
+  end
+  return "HAPPY NEW YEAR!"
 end
